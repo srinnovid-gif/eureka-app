daac1fde522b613f99e71dc2fab5a9e34c3f2528
srinnovid-gif <sr.innovid@gmail.com>
2026-06-15 00:12:02 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -16,7 +16,7 @@
    Cambiar este número fuerza la actualización
    en todos los dispositivos con la app instalada.
 ───────────────────────────────────────── */
-const CACHE_VERSION  = "eureka-v4";
+const CACHE_VERSION  = "eureka-v5";
 
 /* ─────────────────────────────────────────
    ARCHIVOS A CACHEAR
